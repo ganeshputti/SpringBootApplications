efca97084c8ff759ef8341a8310e5c67e79a21f8
ganeshputti <ganeshputti111@gmail.com>
2023-05-31 14:55:30 +0530

    addition of two numbers

---
+++ src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args ) {
+		Integer num1 = 100;
+		Integer num2 = 200;
+		Integer result = 0;
+		
+			result = num1 + num2;
+		System.out.println(result);
+	}
+}
